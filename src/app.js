587a46fa6ab77dc2a311f83bb2469c91fa8e6067
Alperkinali <alperkinali00@hotmail.com>
2021-10-19 00:02:14 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+//değişken tanımlama 
+// JS typesafe değildir.
+let buGun=9.32
+
+
+let dolarDun=9.20
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun= 11.2
+//euroDun = 11
+
+console.log(euroDun)
+let konutKredileri=["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+
+}
+console.log("</ul>")
+console.log(konutKredileri)
